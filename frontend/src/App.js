f5129b35f2205c10d9b72d7f58885aef9070bebc
waguwagu796 <bbosa1012@gmail.com>
2025-11-14 14:54:22 +0900

    scss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import Home from './pages/Home';
 import React, { useEffect } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import WOW from 'wow.js';
 import 'wow.js/css/libs/animate.css';
 import OwlCarousel from 'react-owl-carousel3';
+
 
 function App() {
   useEffect(() => {
