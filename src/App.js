affe58e3b78ae3def8d6d3b6190df921309959ed
waguwagu796 <bbosa1012@gmail.com>
2025-11-14 14:15:16 +0900

    파일 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Home from './pages/Home';
 import React, { useEffect } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import WOW from 'wow.js';
 import 'wow.js/css/libs/animate.css';
