69dd93159706d8333e744fbf84efa7680b51dab8
Trud-Hong <hws7146@naver.com>
2025-11-14 15:43:46 +0900

    수정중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import WOW from 'wow.js';
 import 'wow.js/css/libs/animate.css';
-import OwlCarousel from 'react-owl-carousel3';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import UsersPage from './pages/UsersPage';
 
 
 function App() {
