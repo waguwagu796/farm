f69b8a76f05feb016ca7fd9ecef13ef200307497
waguwagu796 <bbosa1012@gmail.com>
2025-11-14 12:03:55 +0900

    반응

--- src/components/Lib.js
+++ src/components/Lib.js
@@ -1,6 +1,3 @@
-
-import './App.css';
-import Home from './pages/Home';
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
@@ -8,16 +5,15 @@ import WOW from 'wow.js';
 import 'wow.js/css/libs/animate.css';
 import OwlCarousel from 'react-owl-carousel3';
 
-function App() {
-  useEffect(() => {
+const Lib = () => {
+    useEffect(() => {
     new WOW().init();
-  }, []);
+    }, []);
 
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
-}
+    return (
+        <div>
+        </div>
+    );
+};
 
-export default App;
+export default Lib;
